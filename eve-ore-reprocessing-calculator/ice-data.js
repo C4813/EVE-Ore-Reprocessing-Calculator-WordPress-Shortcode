361eefe7b75767e97ab8bbd42cb124c78642a20c
Cable <c4813eve@gmail.com>
2025-07-26 16:20:38 +0100

    Add files via upload

---
+++ eve-ore-reprocessing-calculator/ice-data.js
@@ -0,0 +1,70 @@
+const iceData = {
+  "Blue Ice": {
+    "Heavy Water": 69,
+    "Liquid Ozone": 35,
+    "Strontium Clathrates": 1,
+    "Oxygen Isotopes": 414
+  },
+  "Thick Blue Ice": {
+    "Heavy Water": 104,
+    "Liquid Ozone": 55,
+    "Strontium Clathrates": 1,
+    "Oxygen Isotopes": 483
+  },
+  "Clear Icicle": {
+    "Heavy Water": 69,
+    "Liquid Ozone": 35,
+    "Strontium Clathrates": 1,
+    "Helium Isotopes": 414
+  },
+  "Enriched Clear Icicle": {
+    "Heavy Water": 104,
+    "Liquid Ozone": 55,
+    "Strontium Clathrates": 1,
+    "Helium Isotopes": 483
+  },
+  "Dark Glitter": {
+    "Heavy Water": 691,
+    "Liquid Ozone": 1381,
+    "Strontium Clathrates": 69,
+  },
+  "Gelidus": {
+    "Heavy Water": 345,
+    "Liquid Ozone": 691,
+    "Strontium Clathrates": 104,
+  },
+  "Glacial Mass": {
+    "Heavy Water": 69,
+    "Liquid Ozone": 35,
+    "Strontium Clathrates": 1,
+    "Hydrogen Isotopes": 414
+  },
+  "Smooth Glacial Mass": {
+    "Heavy Water": 104,
+    "Liquid Ozone": 55,
+    "Strontium Clathrates": 1,
+    "Hydrogen Isotopes": 483
+  },
+  "Glare Crust": {
+    "Heavy Water": 1381,
+    "Liquid Ozone": 691,
+    "Strontium Clathrates": 35,
+  },
+  "Krystallos": {
+    "Heavy Water": 173,
+    "Liquid Ozone": 691,
+    "Strontium Clathrates": 173,
+  },
+  "White Glaze": {
+    "Heavy Water": 69,
+    "Liquid Ozone": 35,
+    "Strontium Clathrates": 1,
+    "Nitrogen Isotopes": 414
+  },
+  "Pristine White Glaze": {
+    "Heavy Water": 104,
+    "Liquid Ozone": 55,
+    "Strontium Clathrates": 1,
+    "Nitrogen Isotopes": 483
+  }
+};
---
+++ eve-ore-reprocessing-calculator/ore-data.js
@@ -0,0 +1,648 @@
+const oreData = {
+  "Arkonor": {
+    "Pyerite": 3200.0,
+    "Mexallon": 1200.0,
+    "Megacyte": 120.0
+  },
+  "Crimson Arkonor": {
+    "Pyerite": 3360.0,
+    "Mexallon": 1260.0,
+    "Megacyte": 126.0
+  },
+  "Prime Arkonor": {
+    "Pyerite": 3520.0,
+    "Mexallon": 1320.0,
+    "Megacyte": 132.0
+  },
+  "Flawless Arkonor": {
+    "Pyerite": 3680.0,
+    "Mexallon": 1380.0,
+    "Megacyte": 138.0
+  },
+  "Bezdnacine": {
+    "Tritanium": 40000.0,
+    "Isogen": 4800.0,
+    "Megacyte": 128.0
+  },
+  "Abyssal Bezdnacine": {
+    "Tritanium": 42000.0,
+    "Isogen": 5040.0,
+    "Megacyte": 135.0
+  },
+  "Hadal Bezdnacine": {
+    "Tritanium": 44000.0,
+    "Isogen": 5280.0,
+    "Megacyte": 141.0
+  },
+  "Bistot": {
+    "Pyerite": 3200.0,
+    "Mexallon": 1200.0,
+    "Zydrine": 160.0
+  },
+  "Triclinic Bistot": {
+    "Pyerite": 3360.0,
+    "Mexallon": 1260.0,
+    "Zydrine": 168.0
+  },
+  "Monoclinic Bistot": {
+    "Pyerite": 3520.0,
+    "Mexallon": 1320.0,
+    "Zydrine": 176.0
+  },
+  "Cubic Bistot": {
+    "Pyerite": 3680.0,
+    "Mexallon": 1380.0,
+    "Zydrine": 184.0
+  },
+  "Crokite": {
+    "Pyerite": 800.0,
+    "Mexallon": 2000.0,
+    "Nocxium": 800.0
+  },
+  "Sharp Crokite": {
+    "Pyerite": 840.0,
+    "Mexallon": 2100.0,
+    "Nocxium": 840.0
+  },
+  "Crystalline Crokite": {
+    "Pyerite": 880.0,
+    "Mexallon": 2200.0,
+    "Nocxium": 880.0
+  },
+  "Pellucid Crokite": {
+    "Pyerite": 920.0,
+    "Mexallon": 2300.0,
+    "Nocxium": 920.0
+  },
+  "Dark Ochre": {
+    "Mexallon": 1360.0,
+    "Isogen": 1200.0,
+    "Nocxium": 320.0
+  },
+  "Onyx Ochre": {
+    "Mexallon": 1428.0,
+    "Isogen": 1260.0,
+    "Nocxium": 336.0
+  },
+  "Obsidian Ochre": {
+    "Mexallon": 1496.0,
+    "Isogen": 1320.0,
+    "Nocxium": 352.0
+  },
+  "Jet Ochre": {
+    "Mexallon": 1564.0,
+    "Isogen": 1380.0,
+    "Nocxium": 368.0
+  },
+  "Ducinium": {
+    "Megacyte": 170.0
+  },
+  "Noble Ducinium": {
+    "Megacyte": 179.0
+  },
+  "Royal Ducinium": {
+    "Megacyte": 187.0
+  },
+  "Imperial Ducinium": {
+    "Megacyte": 196.0
+  },
+  "Eifyrium": {
+    "Zydrine": 266.0
+  },
+  "Doped Eifyrium": {
+    "Zydrine": 279.0
+  },
+  "Boosted Eifyrium": {
+    "Zydrine": 293.0
+  },
+  "Augmented Eifyrium": {
+    "Zydrine": 306.0
+  },
+  "Gneiss": {
+    "Pyerite": 2000.0,
+    "Mexallon": 1500.0,
+    "Isogen": 800.0
+  },
+  "Iridescent Gneiss": {
+    "Pyerite": 2100.0,
+    "Mexallon": 1575.0,
+    "Isogen": 840.0
+  },
+  "Prismatic Gneiss": {
+    "Pyerite": 2200.0,
+    "Mexallon": 1650.0,
+    "Isogen": 880.0
+  },
+  "Brilliant Gneiss": {
+    "Pyerite": 2300.0,
+    "Mexallon": 1725.0,
+    "Isogen": 920.0
+  },
+  "Hedbergite": {
+    "Pyerite": 450.0,
+    "Nocxium": 120.0
+  },
+  "Vitric Hedbergite": {
+    "Pyerite": 473.0,
+    "Nocxium": 126.0
+  },
+  "Glazed Hedbergite": {
+    "Pyerite": 495.0,
+    "Nocxium": 132.0
+  },
+  "Lustrous Hedbergite": {
+    "Pyerite": 518.0,
+    "Nocxium": 138.0
+  },
+  "Hemorphite": {
+    "Isogen": 240.0,
+    "Nocxium": 90.0
+  },
+  "Vivid Hemorphite": {
+    "Isogen": 252.0,
+    "Nocxium": 95.0
+  },
+  "Radiant Hemorphite": {
+    "Isogen": 264.0,
+    "Nocxium": 99.0
+  },
+  "Scintillating Hemorphite": {
+    "Isogen": 276.0,
+    "Nocxium": 104.0
+  },
+  "Jaspet": {
+    "Mexallon": 150.0,
+    "Nocxium": 50.0
+  },
+  "Pure Jaspet": {
+    "Mexallon": 158.0,
+    "Nocxium": 53.0
+  },
+  "Pristine Jaspet": {
+    "Mexallon": 165.0,
+    "Nocxium": 55.0
+  },
+  "Immaculate Jaspet": {
+    "Mexallon": 173.0,
+    "Nocxium": 58.0
+  },
+  "Kernite": {
+    "Mexallon": 60.0,
+    "Isogen": 120.0
+  },
+  "Luminous Kernite": {
+    "Mexallon": 63.0,
+    "Isogen": 126.0
+  },
+  "Fiery Kernite": {
+    "Mexallon": 66.0,
+    "Isogen": 132.0
+  },
+  "Resplendant Kernite": {
+    "Mexallon": 69.0,
+    "Isogen": 138.0
+  },
+  "Mercoxit": {
+    "Morphite": 140.0
+  },
+  "Magma Mercoxit": {
+    "Morphite": 147.0
+  },
+  "Vitreous Mercoxit": {
+    "Morphite": 154.0
+  },
+  "Mordunium": {
+    "Pyerite": 88.0
+  },
+  "Plum Mordunium": {
+    "Pyerite": 92.0
+  },
+  "Prize Mordunium": {
+    "Pyerite": 97.0
+  },
+  "Plunder Mordunium": {
+    "Pyerite": 101.0
+  },
+  "Omber": {
+    "Pyerite": 90.0,
+    "Isogen": 75.0
+  },
+  "Silvery Omber": {
+    "Pyerite": 95.0,
+    "Isogen": 79.0
+  },
+  "Golden Omber": {
+    "Pyerite": 99.0,
+    "Isogen": 83.0
+  },
+  "Platinoid Omber": {
+    "Pyerite": 104.0,
+    "Isogen": 87.0
+  },
+  "Plagioclase": {
+    "Tritanium": 174.0,
+    "Mexallon": 70.0
+  },
+  "Azure Plagioclase": {
+    "Tritanium": 183.0,
+    "Mexallon": 74.0
+  },
+  "Rich Plagioclase": {
+    "Tritanium": 192.0,
+    "Mexallon": 77.0
+  },
+  "Sparkling Plagioclase": {
+    "Tritanium": 201.0,
+    "Mexallon": 81.0
+  },
+  "Pyroxeres": {
+    "Pyerite": 90.0,
+    "Mexallon": 30.0
+  },
+  "Solid Pyroxeres": {
+    "Pyerite": 95.0,
+    "Mexallon": 32.0
+  },
+  "Viscous Pyroxeres": {
+    "Pyerite": 99.0,
+    "Mexallon": 33.0
+  },
+  "Opulent Pyroxeres": {
+    "Pyerite": 104.0,
+    "Mexallon": 35.0
+  },
+  "Rakovene": {
+    "Tritanium": 40000.0,
+    "Isogen": 3200.0,
+    "Zydrine": 200.0
+  },
+  "Abyssal Rakovene": {
+    "Tritanium": 42000.0,
+    "Isogen": 3360.0,
+    "Zydrine": 210.0
+  },
+  "Hadal Rakovene": {
+    "Tritanium": 44000.0,
+    "Isogen": 3520.0,
+    "Zydrine": 220.0
+  },
+  "Scordite": {
+    "Tritanium": 150.0,
+    "Pyerite": 99.0
+  },
+  "Condensed Scordite": {
+    "Tritanium": 158.0,
+    "Pyerite": 103.0
+  },
+  "Massive Scordite": {
+    "Tritanium": 165.0,
+    "Pyerite": 110.0
+  },
+  "Glossy Scordite": {
+    "Tritanium": 173.0,
+    "Pyerite": 114.0
+  },
+  "Spodumain": {
+    "Tritanium": 48000.0,
+    "Isogen": 1000.0,
+    "Nocxium": 160.0,
+    "Zydrine": 80.0,
+    "Megacyte": 40.0
+  },
+  "Bright Spodumain": {
+    "Tritanium": 50400.0,
+    "Isogen": 1050.0,
+    "Nocxium": 168.0,
+    "Zydrine": 84.0,
+    "Megacyte": 42.0
+  },
+  "Gleaming Spodumain": {
+    "Tritanium": 52800.0,
+    "Isogen": 1100.0,
+    "Nocxium": 176.0,
+    "Zydrine": 88.0,
+    "Megacyte": 44.0
+  },
+  "Dazzling Spodumain": {
+    "Tritanium": 55200.0,
+    "Isogen": 1150.0,
+    "Nocxium": 184.0,
+    "Zydrine": 92.0,
+    "Megacyte": 46.0
+  },
+  "Talassonite": {
+    "Tritanium": 40000.0,
+    "Nocxium": 960.0,
+    "Megacyte": 32.0
+  },
+  "Abyssal Talassonite": {
+    "Tritanium": 42000.0,
+    "Nocxium": 1008.0,
+    "Megacyte": 34.0
+  },
+  "Hadal Talassonite": {
+    "Tritanium": 44000.0,
+    "Nocxium": 1056.0,
+    "Megacyte": 36.0
+  },
+  "Veldspar": {
+    "Tritanium": 400.0
+  },
+  "Concentrated Veldspar": {
+    "Tritanium": 420.0
+  },
+  "Dense Veldspar": {
+    "Tritanium": 440.0
+  },
+  "Stable Veldspar": {
+    "Tritanium": 460.0
+  },
+  "Ytirium": {
+    "Isogen": 240.0
+  },
+  "Bootleg Ytirium": {
+    "Isogen": 252.0
+  },
+  "Firewater Ytirium": {
+    "Isogen": 264.0
+  },
+  "Moonshine Ytirium": {
+    "Isogen": 276.0
+  },
+  "Bitumens": {
+    "Pyerite": 6000.0,
+    "Mexallon": 400.0,
+    "Hydrocarbons": 65.0
+  },
+  "Brimful Bitumens": {
+    "Pyerite": 6900.0,
+    "Mexallon": 460.0,
+    "Hydrocarbons": 75.0
+  },
+  "Glistening Bitumens": {
+    "Pyerite": 12000.0,
+    "Mexallon": 800.0,
+    "Hydrocarbons": 130.0
+  },
+  "Carnotite": {
+    "Atmospheric Gases": 15.0,
+    "Cobalt": 10.0,
+    "Technetium": 50.0
+  },
+  "Replete Carnotite": {
+    "Atmospheric Gases": 17.0,
+    "Cobalt": 12.0,
+    "Technetium": 58.0
+  },
+  "Glowing Carnotite": {
+    "Atmospheric Gases": 30.0,
+    "Cobalt": 20.0,
+    "Technetium": 100.0
+  },
+  "Chromite": {
+    "Chromium": 40.0,
+    "Hydrocarbons": 10.0
+  },
+  "Lavish Chromite": {
+    "Chromium": 46.0,
+    "Hydrocarbons": 12.0
+  },
+  "Shimmering Chromite": {
+    "Chromium": 80.0,
+    "Hydrocarbons": 20.0
+  },
+  "Cinnabar": {
+    "Evaporite Deposits": 15.0,
+    "Hydrocarbons": 50.0,
+    "Tungsten": 10.0
+  },
+  "Replete Cinnabar": {
+    "Evaporite Deposits": 17.0,
+    "Hydrocarbons": 58.0,
+    "Tungsten": 12.0
+  },
+  "Glowing Cinnabar": {
+    "Evaporite Deposits": 30.0,
+    "Hydrocarbons": 100.0,
+    "Tungsten": 20.0
+  },
+  "Cobaltite": {
+    "Cobalt": 40.0
+  },
+  "Copious Cobaltite": {
+    "Cobalt": 46.0
+  },
+  "Twinkling Cobaltite": {
+    "Cobalt": 80.0
+  },
+  "Coesite": {
+    "Pyerite": 2000.0,
+    "Mexallon": 400.0,
+    "Silicates": 65.0
+  },
+  "Brimful Coesite": {
+    "Pyerite": 2300.0,
+    "Mexallon": 460.0,
+    "Silicates": 75.0
+  },
+  "Glistening Coesite": {
+    "Pyerite": 4000.0,
+    "Mexallon": 800.0,
+    "Silicates": 130.0
+  },
+  "Euxenite": {
+    "Scandium": 40.0
+  },
+  "Copious Euxenite": {
+    "Scandium": 46.0
+  },
+  "Twinkling Euxenite": {
+    "Scandium": 80.0
+  },
+  "Loparite": {
+    "Hydrocarbons": 20.0,
+    "Platinum": 10.0,
+    "Promethium": 22.0,
+    "Scandium": 20.0
+  },
+  "Bountiful Loparite": {
+    "Hydrocarbons": 23.0,
+    "Platinum": 12.0,
+    "Promethium": 25.0,
+    "Scandium": 23.0
+  },
+  "Shining Loparite": {
+    "Hydrocarbons": 40.0,
+    "Platinum": 20.0,
+    "Promethium": 44.0,
+    "Scandium": 40.0
+  },
+  "Monazite": {
+    "Chromium": 10.0,
+    "Evaporite Deposits": 20.0,
+    "Neodymium": 22.0,
+    "Tungsten": 20.0
+  },
+  "Bountiful Monazite": {
+    "Chromium": 12.0,
+    "Evaporite Deposits": 23.0,
+    "Neodymium": 25.0,
+    "Tungsten": 23.0
+  },
+  "Shining Monazite": {
+    "Chromium": 20.0,
+    "Evaporite Deposits": 40.0,
+    "Neodymium": 44.0,
+    "Tungsten": 40.0
+  },
+  "Otavite": {
+    "Atmospheric Gases": 10.0,
+    "Cadmium": 40.0
+  },
+  "Lavish Otavite": {
+    "Atmospheric Gases": 12.0,
+    "Cadmium": 46.0
+  },
+  "Shimmering Otavite": {
+    "Atmospheric Gases": 20.0,
+    "Cadmium": 80.0
+  },
+  "Pollucite": {
+    "Caesium": 50.0,
+    "Hydrocarbons": 15.0,
+    "Scandium": 10.0
+  },
+  "Replete Pollucite": {
+    "Caesium": 58.0,
+    "Hydrocarbons": 17.0,
+    "Scandium": 12.0
+  },
+  "Glowing Pollucite": {
+    "Caesium": 100.0,
+    "Hydrocarbons": 30.0,
+    "Scandium": 20.0
+  },
+  "Scheelite": {
+    "Tungsten": 40.0
+  },
+  "Copious Scheelite": {
+    "Tungsten": 46.0
+  },
+  "Twinkling Scheelite": {
+    "Tungsten": 80.0
+  },
+  "Sperrylite": {
+    "Evaporite Deposits": 10.0,
+    "Platinum": 40.0
+  },
+  "Lavish Sperrylite": {
+    "Evaporite Deposits": 12.0,
+    "Platinum": 46.0
+  },
+  "Shimmering Sperrylite": {
+    "Evaporite Deposits": 20.0,
+    "Platinum": 80.0
+  },
+  "Sylvite": {
+    "Pyerite": 4000.0,
+    "Mexallon": 400.0,
+    "Evaporite Deposits": 65.0
+  },
+  "Brimful Sylvite": {
+    "Pyerite": 4600.0,
+    "Mexallon": 460.0,
+    "Evaporite Deposits": 75.0
+  },
+  "Glistening Sylvite": {
+    "Pyerite": 8000.0,
+    "Mexallon": 800.0,
+    "Evaporite Deposits": 130.0
+  },
+  "Titanite": {
+    "Titanium": 40.0
+  },
+  "Copious Titanite": {
+    "Titanium": 46.0
+  },
+  "Twinkling Titanite": {
+    "Titanium": 80.0
+  },
+  "Vanadinite": {
+    "Silicates": 10.0,
+    "Vanadium": 40.0
+  },
+  "Lavish Vanadinite": {
+    "Silicates": 12.0,
+    "Vanadium": 46.0
+  },
+  "Shimmering Vanadinite": {
+    "Silicates": 20.0,
+    "Vanadium": 80.0
+  },
+  "Xenotime": {
+    "Atmospheric Gases": 20.0,
+    "Cobalt": 20.0,
+    "Dysprosium": 22.0,
+    "Vanadium": 10.0
+  },
+  "Bountiful Xenotime": {
+    "Atmospheric Gases": 23.0,
+    "Cobalt": 23.0,
+    "Dysprosium": 25.0,
+    "Vanadium": 12.0
+  },
+  "Shining Xenotime": {
+    "Atmospheric Gases": 40.0,
+    "Cobalt": 40.0,
+    "Dysprosium": 44.0,
+    "Vanadium": 20.0
+  },
+  "Ytterbite": {
+    "Cadmium": 10.0,
+    "Silicates": 20.0,
+    "Thulium": 22.0,
+    "Titanium": 20.0
+  },
+  "Bountiful Ytterbite": {
+    "Cadmium": 12.0,
+    "Silicates": 23.0,
+    "Thulium": 25.0,
+    "Titanium": 23.0
+  },
+  "Shining Ytterbite": {
+    "Cadmium": 20.0,
+    "Silicates": 40.0,
+    "Thulium": 44.0,
+    "Titanium": 40.0
+  },
+  "Zeolites": {
+    "Pyerite": 8000.0,
+    "Mexallon": 400.0,
+    "Atmospheric Gases": 65.0
+  },
+  "Brimful Zeolites": {
+    "Pyerite": 9200.0,
+    "Mexallon": 460.0,
+    "Atmospheric Gases": 75.0
+  },
+  "Glistening Zeolite": {
+    "Pyerite": 16000.0,
+    "Mexallon": 800.0,
+    "Atmospheric Gases": 130.0
+  },
+  "Zircon": {
+    "Hafnium": 50.0,
+    "Silicates": 15.0,
+    "Titanium": 10.0
+  },
+  "Replete Zircon": {
+    "Hafnium": 58.0,
+    "Silicates": 17.0,
+    "Titanium": 12.0
+  },
+  "Glowing Zircon": {
+    "Hafnium": 100.0,
+    "Silicates": 30.0,
+    "Titanium": 20.0
+  }
+};
